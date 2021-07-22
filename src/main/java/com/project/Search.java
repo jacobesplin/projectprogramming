cd712272346992dca2c3238048c90803b57f3628
jacobesplin <39013325+jacobesplin@users.noreply.github.com>
2021-07-22 16:50:14 -0600

    Update Search.java

--- src/main/java/com/project/Search.java
+++ src/main/java/com/project/Search.java
@@ -2,10 +2,11 @@ package com.project;
 
 public class Search{
     public static String search(String q){
-        if(q.contains("home"))return "redirect:/index";
+        if(q.contains("home"))return "redirect:/";
         if(q.contains("contact"))return "redirect:/contact";
         if(q.contains("appointment"))return "redirect:/appointment";
         if(q.contains("about"))return "redirect:/about";
+        if(q.contains("minecraft") || q.contains("minecraftserver"))return "redirect:/minecraftserver";
 
         return "searchFailed";
     }
