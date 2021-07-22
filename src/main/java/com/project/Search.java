cdc06844da4d678c2d3bd63a35a5f35cd870d70f
jacobesplin <39013325+jacobesplin@users.noreply.github.com>
2021-07-22 16:55:56 -0600

    Update Search.java

--- src/main/java/com/project/Search.java
+++ src/main/java/com/project/Search.java
@@ -7,6 +7,7 @@ public class Search{
         if(q.contains("appointment"))return "redirect:/appointment";
         if(q.contains("about"))return "redirect:/about";
         if(q.contains("minecraft") || q.contains("minecraftserver"))return "redirect:/minecraftserver";
+        if(q.contains("request") || q.contains("approve"))return "redirect:/approve";
 
         return "searchFailed";
     }
