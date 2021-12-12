4b1808b3269c38ba6220c7ce08e566da8bb9de4c
jacobesplin <39013325+jacobesplin@users.noreply.github.com>
2021-12-12 15:18:36 -0700

    replace spaces

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -129,7 +129,7 @@ public class Main {
   @RequestMapping(value = "/languages/{urlParameter}")
   String viewProject(@RequestParam("project") String project, Map<String, Object> model) {
     API api = new API();
-    String urlApi = "https://appsolutions.pythonanywhere.com/api/v12/data/html/" + project;
+    String urlApi = "https://appsolutions.pythonanywhere.com/api/v12/data/html/" + project.replace(" ", "");
     String html = api.pullData(urlApi);
     model.put("body", html);
     // System.out.println(project);
