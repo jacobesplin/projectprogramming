ca2589a399a801090fa60fffb75a005c268280ec
jacobesplin <39013325+jacobesplin@users.noreply.github.com>
2021-12-12 15:57:18 -0700

    treacker

--- src/main/java/com/project/TrackVisitors.java
+++ src/main/java/com/project/TrackVisitors.java
@@ -11,19 +11,20 @@ class TrackVisitors {
 			@Override
 			public void run() {
 				try {
-					APISelfSigned api_self = new APISelfSigned();
-					api_self.sendDataToServer("https://jacobsmuzik.ddns.net/ReactSpringBoot/api/v12/data/collection",
-							"{\"location\":\"projprog\",\"ip\":\"" + ipaddr + "\"}");
-				} catch (Exception e) {
-
-				}
-				try {
 					API api = new API();
 					api.postData("https://appsolutions.pythonanywhere.com/api/v12/data/projectprogramming/visitors",
 							"{\"location\":\"projprog\",\"ip\":\"" + ipaddr + "\"}");
 				} catch (Exception e2) {
 
 				}
+				try {
+					APISelfSigned api_self = new APISelfSigned();
+					api_self.sendDataToServer("https://jacobsmuzik.ddns.net/ReactSpringBoot/api/v12/data/collection",
+							"{\"location\":\"projprog\",\"ip\":\"" + ipaddr + "\"}");
+				} catch (Exception e) {
+
+				}
+
 			}
 		};
 		Thread track = new Thread(tracker);
