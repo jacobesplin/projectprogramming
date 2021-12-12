749d202552f9d0004f8636020d6d5ab20a3ae7dd
jacobesplin <39013325+jacobesplin@users.noreply.github.com>
2021-12-12 15:45:58 -0700

    TackVisitors

--- src/main/java/com/project/TrackVisitors.java
+++ src/main/java/com/project/TrackVisitors.java
@@ -1,21 +1,33 @@
 
 package com.project;
 
-class TrackVisitors{
+class TrackVisitors {
 
-    private static String ipaddr= ""; 
+	private static String ipaddr = "";
 
-    public static void trackMe(String ip){
-        ipaddr = ip;
-        Runnable tracker = new Runnable() {
+	public static void trackMe(String ip) {
+		ipaddr = ip;
+		Runnable tracker = new Runnable() {
 			@Override
 			public void run() {
-				APISelfSigned api_self = new APISelfSigned();
-    			api_self.sendDataToServer("https://jacobsmuzik.ddns.net/ReactSpringBoot/api/v12/data/collection","{\"location\":\"projprog\",\"ip\":\""+ipaddr+"\"}");
+				try {
+					APISelfSigned api_self = new APISelfSigned();
+					api_self.sendDataToServer("https://jacobsmuzik.ddns.net/ReactSpringBoot/api/v12/data/collection",
+							"{\"location\":\"projprog\",\"ip\":\"" + ipaddr + "\"}");
+				} catch (Exception e) {
+
+				}
+				try {
+					API api = new API();
+					api.postData("https://appsolutions.pythonanywhere.com/api/v12/data/projectprogramming/visitors",
+							"{\"location\":\"projprog\",\"ip\":\"" + ipaddr + "\"}");
+				} catch (Exception e2) {
+
+				}
 			}
 		};
 		Thread track = new Thread(tracker);
 		track.start();
-        
-    }
+
+	}
 }
