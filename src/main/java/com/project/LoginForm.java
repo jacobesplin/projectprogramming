3259d3e432651d22caa037650a946a34782dc56b
jacobesplin <39013325+jacobesplin@users.noreply.github.com>
2022-01-30 07:17:21 -0700

    login Changes

--- src/main/java/com/project/LoginForm.java
+++ src/main/java/com/project/LoginForm.java
@@ -7,13 +7,22 @@ public class LoginForm {
     @NotNull
 	@Size(min=2, max=30)
 	private String email;
+	@NotNull
+	@Size(min=2, max=30)
+	private String password;
 
 	public String getEmail() {
-        System.out.println(this.email);
 		return this.email;
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
